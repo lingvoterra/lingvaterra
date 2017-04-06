6e0eafe9c67ac303b3986f01341cad318d777de1
Igor <igorshing@gmail.com>
2017-04-06 13:06:43 +0300

    Renew the project

---
+++ src/main/java/com/lingvaterra/App.java
@@ -0,0 +1,13 @@
+package com.lingvaterra;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
